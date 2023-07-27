14d957e9d948d17c5b036286b8646ca7a9677041
Ajaymagar <magara18extc@student.mes.ac.in>
2023-07-27 23:36:30 +0530

    First Commitm

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          personal blog coming
         </a>
       </header>
     </div>
